fadbccfda7cc90ec5c5253e2dac73dbcc2c525a1
Refal Alharbi <108233127+Refal22@users.noreply.github.com>
2024-10-06 10:57:30 +0300

    Update GreenFlow_App.py

--- GreenFlow_App.py
+++ GreenFlow_App.py
@@ -14,9 +14,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+cfg_enable_url_download = True
+url = "https://archive.org/download/yolo-model-1/YOLO_Model%20%281%29.pt"
 
-# Load the YOLO model
-model = YOLO("C:\\Users\\Ref42\\OneDrive\\سطح المكتب\\Capestone_Project\\YOLO_Model (1).pt")
+model = YOLO("YOLO_Model (1).pt")
 
 # Streamlit page configuration
 st.set_page_config(
